441c2bda2a242887ea31ff318c3468e5e2460619
c4fun <pcfeng502@gmail.com>
2024-02-19 12:41:32 +0800

    使用saas列表对于仓库名字进行切割

--- llm_project_helper/repo_traverser.py
+++ llm_project_helper/repo_traverser.py
@@ -6,22 +6,37 @@ from llm_project_helper.parser.python_parser import python_analyze_code
 from loguru import logger
 from llm_project_helper.analyzer.file_summary_analyzer import FileSummaryAnalyzer
 from llm_project_helper.analyzer.code_section_analyzer import CodeSectionAnalyzer
-from llm_project_helper.const import TREE_JSON, FORCE_RE_ANALYZE, FORCE_RE_COMMENT
+from llm_project_helper.const import TREE_JSON, FORCE_RE_ANALYZE, FORCE_RE_COMMENT, AVAILABLE_SAAS, WORKSPACE_DIR
+
+load_dotenv()
 
 
 class RepoTraverser:
     def __init__(self):
-        load_dotenv()
         self.repo_path = os.getenv("REPO_PATH")
 
-    def get_cur_ws_dir(self, workspaces_dir):
+    def get_cur_ws_dir(self):
         if not self.repo_path:
             raise ValueError("Repository path not configured in .env file")
+        workspaces_dir = WORKSPACE_DIR
+        logger.info(f"workspaces_dir: {workspaces_dir}")
 
         # get the last part of the repo path, seperated by '/' or '\'
         repo_name = self.repo_path.split(os.sep)[-1]
         if len(repo_name) == 0:
             repo_name = self.repo_path.split(os.sep)[-2]
+
+        # REPO_PATH="/home/richardliu/code/github.com/LargeWorldModel/LWM"
+        # find if REPO_PATH contains aviailable SAAS. If so, then use the last part of the path(including the avaible saas) as the repo_name
+        for saas in AVAILABLE_SAAS:
+            if saas in self.repo_path:
+                logger.info(f"repo_path contains saas: {saas}")
+                repo_name = self.repo_path.split(saas)[-1]
+                repo_name = saas + repo_name
+                break
+        
+        logger.info(f"repo_name from get_cur_ws_dir: {repo_name}")
+
         cur_ws_dir = os.path.join(workspaces_dir, repo_name)
         # create workspaces_dir if not exists
         if not os.path.exists(cur_ws_dir):
@@ -29,8 +44,8 @@ class RepoTraverser:
             os.makedirs(cur_ws_dir)
         return cur_ws_dir
 
-    def traverse_repo(self, workspaces_dir):
-        cur_ws_dir = self.get_cur_ws_dir(workspaces_dir)
+    def traverse_repo(self):
+        cur_ws_dir = self.get_cur_ws_dir()
 
         for root, dirs, files in os.walk(self.repo_path):
             for file in files:
@@ -55,17 +70,17 @@ class RepoTraverser:
                         folder_path = relative_path.replace(py_path, '')
 
                         cur_file_path = os.path.join(cur_ws_dir, folder_path)
-                        src_file_path = os.path.join(self.repo_path, folder_path)
+                        # src_file_path = os.path.join(self.repo_path, folder_path)
                         if not os.path.exists(cur_file_path):
                             logger.debug(f'Creating folder: {cur_file_path}')
                             os.makedirs(cur_file_path)
                         json_file = os.path.join(cur_file_path, py_path + '.json')
 
-                        # 目前是临时的直接拷贝，后续需要通过规则获取源文件，或者记录在json文件中
-                        # TODO: 在sectioned_comment通过规则获取源文件
-                        src_file = os.path.join(src_file_path, py_path)
-                        # copy the src_file to cur_file_path
-                        shutil.copy2(src_file, os.path.join(cur_file_path, py_path))
+                        # # 目前是临时的直接拷贝，后续需要通过规则获取源文件，或者记录在json文件中
+                        # # TODO: 在sectioned_comment通过规则获取源文件
+                        # src_file = os.path.join(src_file_path, py_path)
+                        # # copy the src_file to cur_file_path
+                        # shutil.copy2(src_file, os.path.join(cur_file_path, py_path))
 
                     with open(os.path.join(cur_ws_dir, json_file), 'w') as f:
                         json.dump(output, f, indent=4)
@@ -115,12 +130,18 @@ class RepoTraverser:
                 summary_file = file_path.replace('.json', '.analyze.md')
                 # get relevant code file:
                 code_file = file_path.replace('.json', '')
+                local_repo_folder = os.getenv("LOCAL_REPO_FOLDER")
+                # # if local_repo_folder does not have trailing os.sep, add it. AS WORKSPACE_DIR does not have trailing os.sep, this is not needed
+                # if local_repo_folder[-1] != os.sep:
+                #     local_repo_folder += os.sep
+                code_file = code_file.replace(WORKSPACE_DIR, local_repo_folder)
+                logger.info(f"code_file: {code_file}")
 
                 code_section_analyzer = CodeSectionAnalyzer()
                 comments = code_section_analyzer.analyze_code_section(file_path, summary_file, code_file)
                 logger.info(f"Comments of file {code_file} is:\n {comments}")
                 result = {
-                    "file_path": code_file,
+                    "file_path": code_file,  # TODO: 这里的code_file需要使用SaaS地址+群组+项目+文件名方式
                     "comments": comments
                 }
                 with open(analyze_file, 'w', encoding='utf-8') as f:
--- main.py
+++ main.py
@@ -2,24 +2,22 @@ import os
 
 from llm_project_helper import RepoTraverser
 from llm_project_helper.logs import logger
-import llm_project_helper.const
 
 from dotenv import load_dotenv
 load_dotenv()
 
-WORKSPACE_DIR = "workspaces"
 
 if __name__ == '__main__':
     traverser = RepoTraverser()
 
-    # concat current folder with workspaces; if env defined a home folder, then use that
-    workspaces_dir = os.path.join(os.getcwd(), WORKSPACE_DIR)
-    if (llm_project_helper.const.HOME_FOLDER_WORKSPACE_FLAG):
-        workspaces_dir = llm_project_helper.const.WORKSPACE_DIR
-    logger.info(f"workspaces_dir: {workspaces_dir}")
+    # # concat current folder with workspaces; if env defined a home folder, then use that
+    # workspaces_dir = os.path.join(os.getcwd(), WORKSPACE_DIR)
+    # if (llm_project_helper.const.HOME_FOLDER_WORKSPACE_FLAG):
+    #     workspaces_dir = llm_project_helper.const.WORKSPACE_DIR
+    # logger.info(f"workspaces_dir: {workspaces_dir}")
 
     # 1. Doing the structure analyzation. LLM is not USED HERE
-    analyze_folder = traverser.traverse_repo(workspaces_dir)
+    analyze_folder = traverser.traverse_repo()
     logger.info(f"Analyze folder: {analyze_folder}")
 
     # 2. Traverse and output the xxx.py.analyze.md file using LLM
