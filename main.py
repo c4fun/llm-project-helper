4983b82d96934248437ee94dbbeba741e1f28bef
c4fun <pcfeng502@gmail.com>
2024-03-16 09:38:54 +0800

    update the force-re-analyze and force-re-comment method. Update logging method

--- main.py
+++ main.py
@@ -14,14 +14,31 @@ if __name__ == '__main__':
         type=str,
         help="The code repo path to analyze"
     )
-    
+    # add an argument called forced-re-analyze, which is a boolean,
+    # and default is false, but if it is set, then it is true
+    parser.add_argument(
+        "--force-re-analyze",
+        action="store_true",
+        help="Force re-analyze the whole repo"
+    )
+    # add an argument called forced-re-comment, which is a boolean,
+    # and default is false, but if it is set, then it is true
+    parser.add_argument(
+        "--force-re-comment",
+        action="store_true",
+        help="Force re-comment the whole repo"
+    )
+
     args = parser.parse_args()
     repo_path = args.repo_path
-    print(f"Repo path: {repo_path}")
+    logger.info(f"Repo path: {repo_path}")
     # if the args does not have a repo_path, provide one
     if not repo_path:
         sys.exit("Please provide a repo path in --repo-path. Exiting")
-
+    force_re_analyze = args.force_re_analyze
+    logger.info(f"Force re-analyze: {force_re_analyze}")
+    force_re_comment = args.force_re_comment
+    logger.info(f"Force re-comment: {force_re_comment}")
     traverser = RepoTraverser(repo_path)
 
     # # concat current folder with workspaces; if env defined a home folder, then use that
@@ -35,7 +52,7 @@ if __name__ == '__main__':
     logger.info(f"Analyze folder: {analyze_folder}")
 
     # 2. Traverse and output the xxx.py.analyze.md file using LLM
-    traverser.analyze_repo(analyze_folder)
+    traverser.analyze_repo(analyze_folder, force_re_analyze)
 
     # 3. Analyze code section by section and output to xxx.py.comments.json
-    traverser.sectioned_comment(analyze_folder)
+    traverser.sectioned_comment(analyze_folder, force_re_comment)
