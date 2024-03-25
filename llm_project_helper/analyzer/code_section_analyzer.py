19e7a5d0a5bc96a9aaf3867b29654cc10b5853ac
c4fun <pcfeng502@gmail.com>
2024-03-25 10:16:43 +0800

    added comment for classes

--- llm_project_helper/analyzer/code_section_analyzer.py
+++ llm_project_helper/analyzer/code_section_analyzer.py
@@ -1,7 +1,7 @@
 import json
 from loguru import logger
 from llm_project_helper.provider import ZhipuAIAPI
-from llm_project_helper.const import CODE_SECTION_PROMPT_JSON as PROMPT
+from llm_project_helper.const import CODE_SECTION_PROMPT_JSON as PROMPT, CODE_CLASS_PROMPT_JSON
 
 
 class CodeSectionAnalyzer:
@@ -16,6 +16,7 @@ class CodeSectionAnalyzer:
             summary = file.read()
             logger.debug(f"Summary is: \n{summary}")
             self.prompt = self.prompt.replace("```markdown```", "```markdown\n" + summary + "\n```")
+            class_prompt = CODE_CLASS_PROMPT_JSON.replace("```markdown```", "```markdown\n" + summary + "\n```")
 
         # 2. Get every section(methods, functions) of the code through the json_file_path
         with open(json_file_path, 'r') as file:
@@ -44,6 +45,7 @@ class CodeSectionAnalyzer:
             for class_name, class_details in data['classes'].items():
                 logger.info(f"Class: {class_name}")
                 class_pesudo_code = f"Class {class_name}:\n"
+                class_descendant_remarks = ""
                 # 3. open code_file_path and only read relevant lines
                 if 'methods' in class_details:
                     method_pairs = self.process_functions(class_details['methods'])
@@ -62,6 +64,20 @@ class CodeSectionAnalyzer:
                         logger.info(f"Remark returned from LLM is: \n{remark}")
                         # 6. add remarks and line numbers to the comments
                         comments.append({"line_no": start_line, "remark": remark})
+                        
+                        # 7. affliate info in class_descendant_remarks
+                        class_descendant_remarks += f"从 {start_line} 开始的方法:\n{remark}\n"
+
+                # 8. Get class summary info
+                class_prompt = class_prompt.replace(
+                    "[|$|class_descendant_remarks|$|]", class_descendant_remarks)
+                logger.debug(f"Class Prompt is: \n{class_prompt}")
+                class_chat_result = self.api.predict(class_prompt)
+                class_remark = class_chat_result.content
+                logger.info(f"Class Remark returned from LLM is: \n{class_remark}")
+                # 9. add class remarks and line numbers to the comments
+                comments.append({"line_no": class_details["line_number"], "remark": class_remark})
+
 
         # 7. Return the comments
         return comments
